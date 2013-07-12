be58a4ed5112ce0f254f0107e4ce8e6c6db0e8bd
wblew <wblew@linode.com>
2013-07-12 10:57:29 -0400

    iride v2

--- src/com/westeroscraft/iRide/Main.java
+++ src/com/westeroscraft/iRide/Main.java
@@ -24,8 +24,16 @@ public class Main extends JavaPlugin implements Listener {
         log("iRide is now enabled.");	
         
         ShapedRecipe saddle = new ShapedRecipe(new ItemStack(Material.SADDLE,1)).shape("***", "S*S", "I I").setIngredient('*', Material.LEATHER).setIngredient('S', Material.STRING).setIngredient('I', Material.IRON_INGOT);
-   //add the shizz
+        ShapedRecipe nametag = new ShapedRecipe(new ItemStack(Material.NAME_TAG,1)).shape(" I ", " I ", " B ").setIngredient('B', Material.BLAZE_ROD).setIngredient('I', Material.IRON_INGOT);
+        ShapedRecipe ghorsearmor = new ShapedRecipe(new ItemStack(418)).shape("***", "*G*", "G G").setIngredient('*', Material.LEATHER).setIngredient('G', Material.GOLD_INGOT);
+        ShapedRecipe ihorsearmor = new ShapedRecipe(new ItemStack(417)).shape("***", "*I*", "I I").setIngredient('*', Material.LEATHER).setIngredient('I', Material.IRON_INGOT);
+        ShapedRecipe dhorsearmor = new ShapedRecipe(new ItemStack(419)).shape("***", "*D*", "D D").setIngredient('*', Material.LEATHER).setIngredient('D', Material.DIAMOND);
+        //add the shizz
         getServer().addRecipe(saddle);
+        getServer().addRecipe(nametag);
+        getServer().addRecipe(ghorsearmor);
+        getServer().addRecipe(ihorsearmor);
+        getServer().addRecipe(dhorsearmor);
     }
     //end ons
     //logger
